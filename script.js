ad630a18bf59500b4d99894f0d74d4f3e8535a79
Cherine Rafla <cherinerafla@Cherines-MacBook-Air.local>
2017-10-23 23:33:46 -0400

    fixed positioning for phone view

--- script.js
+++ script.js
@@ -3,49 +3,16 @@ drive();
 function drive() {
     $(document).ready(function() {
         $("#history_button").click(function() {
-            $("#language_arts").hide(300);
-            $("#language_arts_button").show();
+            $("#Random").hide(300);
+            $("#random_folder_button").show();
             $("#history").show(300);
             $("#history_button").hide();
         });
-        $("#language_arts_button").click(function() {
+        $("#random_folder_button").click(function() {
             $("#history").hide(300);
             $("#history_button").show();
-            $("#language_arts").show(300);
-            $("#language_arts_button").hide();
+            $("#Random").show(300);
+            $("#random_folder_button").hide();
         });
-    });
-}
-
-RemoveJumping();
-
-function RemoveJumping() {
-    $("#tab").click(function(e) { //Note the e for event
-        e.preventDefault();
-      });
-}
-
-
-RemoveHash();
-
-function RemoveHash() {
-    (function($) {
-        $('#tab').on('click', function(evt) {
-            evt.preventDefault(); //prevents hash from being append to the url
-        });
-        (window.jQuery);
-    });
-}
-
-SmootherScroll();
-
-function SmootherScroll() {
-    $(window).scroll(function() {
-        var newTop = $(window).scrollTop();
-        if (newTop <= 130) {
-            newTop = 130;
-        }
-        $("#idofElementToScroll").stop()
-            .animate({ 'top': newTop }, "slow");
     });
 }
